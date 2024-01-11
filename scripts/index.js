e25cf5f6870956631056ac1db4263faeccdd2a94
DavidKhlutkov <davidhlutkov2k18@gmail.com>
2024-01-11 16:38:21 +0300

    add script card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+// Функция добавления темплейта
+function createCard(data, deleteCallBack) {
+// Создание темплейта
+  const cardTemplate = document.querySelector("#card-template");
+  const cardElement = cardTemplate.content.querySelector(".places__item").cloneNode(true);
+// 
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+// Заполняем атрибуты картинки и текста данными
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
 
-// @todo: DOM узлы
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", function() {
+    deleteCallBack(cardElement);
+  });
 
-// @todo: Функция создания карточки
+  return cardElement;
+}
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+// Функция с циклом выведения карточек на страницу
+function renderCards() {
+  const placesList = document.querySelector(".places__list");
 
-// @todo: Функция удаления карточки
+  for (let i = 0; i < initialCards.length; i++) {
+    const cardElement = createCard(initialCards[i], deleteCard);
+    placesList.appendChild(cardElement);
+  }
+}
 
-// @todo: Вывести карточки на страницу
+renderCards();
